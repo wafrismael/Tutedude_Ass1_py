392a1ef30dced33c82aee8f8e0fb2205fb09ff2a
WAFRISMAEL <wafar_22b450cs@gecwyd.ac.in>
2025-03-10 19:55:51 +0530

    Add files via upload

---
+++ tutedude ass1.2.py
@@ -0,0 +1,4 @@
+f_name=input("Enter the First name")
+l_name=input("Enter the last name")
+
+print("Hello,"+f_name+" "+l_name+" Welcome to python")
---
+++ tutedude ass1.py
@@ -0,0 +1,13 @@
+
+num1=float(input("Enter the first number "))
+num2=float(input("Enter the second number "))
+
+sum = num1 + num2
+diff = num1 - num2
+mul = num1 * num2
+div = num1 - num2
+print("Results:\n")
+print("Addition\t:",sum)
+print("substraction\t:",diff)
+print("multiplication\t:",mul)
+print("division\t:",div)
